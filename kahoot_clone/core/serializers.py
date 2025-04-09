06ae8f2d2a73a195630c783847e20e070d12ce55
Taalaibekovi4 <206237128+SaliRasul@users.noreply.github.com>
2025-04-09 15:05:55 +0600

    2
    2

--- kahoot_clone/core/serializers.py
+++ kahoot_clone/core/serializers.py
@@ -1,10 +1,56 @@
+# from rest_framework import serializers
+# from .models import *
+#
+# class StudentSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Student
+#         fields = ['name']
+#
+# class ClassroomSerializer(serializers.ModelSerializer):
+#     students = StudentSerializer(many=True)
+#
+#     class Meta:
+#         model = Classroom
+#         fields = ['id', 'name', 'students']
+#
+# class LevelSerializer(serializers.ModelSerializer):
+#     participants = StudentSerializer(many=True)
+#
+#     class Meta:
+#         model = Level
+#         fields = ['id', 'title', 'participants']
+#
+# class SchoolSerializer(serializers.ModelSerializer):
+#     classes = ClassroomSerializer(many=True)
+#     levels = LevelSerializer(many=True)
+#
+#     class Meta:
+#         model = School
+#         fields = ['id', 'name', 'classes', 'levels']
+#
+# class WinnerEntrySerializer(serializers.ModelSerializer):
+#     student = StudentSerializer()
+#
+#     class Meta:
+#         model = Winner
+#         fields = ['student', 'score']
+#
+# class QuizSerializer(serializers.ModelSerializer):
+#     winners = WinnerEntrySerializer(many=True)
+#     level = serializers.StringRelatedField()
+#
+#     class Meta:
+#         model = Quiz
+#         fields = ['id', 'quiz_name', 'level', 'date', 'winners']
 from rest_framework import serializers
-from .models import *
+from .models import Student, School, Classroom, Level, Quiz, Winner
+
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ['name']
+
 
 class ClassroomSerializer(serializers.ModelSerializer):
     students = StudentSerializer(many=True)
@@ -13,12 +59,14 @@ class ClassroomSerializer(serializers.ModelSerializer):
         model = Classroom
         fields = ['id', 'name', 'students']
 
+
 class LevelSerializer(serializers.ModelSerializer):
     participants = StudentSerializer(many=True)
 
     class Meta:
         model = Level
         fields = ['id', 'title', 'participants']
+
 
 class SchoolSerializer(serializers.ModelSerializer):
     classes = ClassroomSerializer(many=True)
@@ -28,17 +76,31 @@ class SchoolSerializer(serializers.ModelSerializer):
         model = School
         fields = ['id', 'name', 'classes', 'levels']
 
+
 class WinnerEntrySerializer(serializers.ModelSerializer):
-    student = StudentSerializer()
+    name = serializers.CharField(source='student.name')
 
     class Meta:
         model = Winner
-        fields = ['student', 'score']
+        fields = ['name', 'score']
+
 
 class QuizSerializer(serializers.ModelSerializer):
-    winners = WinnerEntrySerializer(many=True)
+    winners = serializers.SerializerMethodField()
     level = serializers.StringRelatedField()
+    quiz_name = serializers.CharField(source='name')
 
     class Meta:
         model = Quiz
         fields = ['id', 'quiz_name', 'level', 'date', 'winners']
+
+    def get_winners(self, obj):
+        winners = obj.winners.all()
+        if winners.exists():
+            return WinnerEntrySerializer(winners, many=True).data
+        else:
+            return [
+                {"name": "", "score": None},
+                {"name": "", "score": None},
+                {"name": "", "score": None}
+            ]
